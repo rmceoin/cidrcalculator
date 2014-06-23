9cd439ff398acadb75e6982d17f88be1aa940b1c
rmceoin <1653739+rmceoin@users.noreply.github.com>
2014-06-23 11:28:34 -0700

    Fixed HistoryList, which was not showing any data.  Added LoadManager calls.

--- app/src/main/java/us/lindanrandy/cidrcalculator/HistoryProvider.java
+++ app/src/main/java/us/lindanrandy/cidrcalculator/HistoryProvider.java
@@ -1,7 +1,6 @@
 /*
- * $Id: HistoryProvider.java 34 2012-02-10 05:05:20Z rmceoin@gmail.com $
- *  
- * Copyright (C) 2009 Randy McEoin
+ *
+ * Copyright (C) 2009-2014 Randy McEoin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,10 +18,12 @@
 package us.lindanrandy.cidrcalculator;
 
 import java.util.HashMap;
+import java.util.List;
 
 import us.lindanrandy.cidrcalculator.CIDRHistory.History;
 
 import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
@@ -42,14 +43,14 @@ import android.util.Log;
 
 public class HistoryProvider extends ContentProvider {
 
-	private static final String TAG = "History";
-	private static final boolean debug = false;
+    private static final String TAG = "History";
+    private static final boolean debug = false;
 
-	private static final String DATABASE_NAME = "history.db";
-	private static final int DATABASE_VERSION = 1;
-	private static final String HISTORY_TABLE_NAME = "history";
+    private static final String DATABASE_NAME = "history.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String HISTORY_TABLE_NAME = "history";
 
-	private static HashMap<String, String> sHistoryProjectionMap;
+    private static HashMap<String, String> sHistoryProjectionMap;
 
     private static final int HISTORY = 1;
     private static final int HISTORY_ID = 2;
@@ -57,251 +58,279 @@ public class HistoryProvider extends ContentProvider {
     private static final UriMatcher sUriMatcher;
 
 
-	/**
-	 * This class helps open, create, and upgrade the database file.
-	 */
-	private static class DatabaseHelper extends SQLiteOpenHelper {
+    /**
+     * This class helps open, create, and upgrade the database file.
+     */
+    private static class DatabaseHelper extends SQLiteOpenHelper {
 
-		DatabaseHelper(Context context) {
-			super(context, DATABASE_NAME, null, DATABASE_VERSION);
-		}
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
 
-		@Override
-		public void onCreate(SQLiteDatabase db) {
-			db.execSQL("CREATE TABLE " + HISTORY_TABLE_NAME + " ("
-					+ History._ID + " INTEGER PRIMARY KEY,"
-					+ History.IP + " TEXT,"
-					+ History.BITS + " INTEGER,"
-					+ History.CREATED_DATE + " INTEGER,"
-					+ History.MODIFIED_DATE + " INTEGER"
-					+ ");");
-		}
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE " + HISTORY_TABLE_NAME + " ("
+                    + History._ID + " INTEGER PRIMARY KEY,"
+                    + History.IP + " TEXT,"
+                    + History.BITS + " INTEGER,"
+                    + History.CREATED_DATE + " INTEGER,"
+                    + History.MODIFIED_DATE + " INTEGER"
+                    + ");");
+        }
 
-		@Override
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-			Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
-					+ newVersion + ", which will destroy all old data");
-			db.execSQL("DROP TABLE IF EXISTS " + HISTORY_TABLE_NAME);
-			onCreate(db);
-		}
-	}
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS " + HISTORY_TABLE_NAME);
+            onCreate(db);
+        }
+    }
 
-	private DatabaseHelper mOpenHelper;
+    private DatabaseHelper mOpenHelper;
 
-	@Override
-	public boolean onCreate() {
-		mOpenHelper = new DatabaseHelper(getContext());
-		return true;
-	}	
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new DatabaseHelper(getContext());
+        return true;
+    }
 
-	@Override
-	public Cursor query(Uri uri, String[] projection, String selection,
-			String[] selectionArgs, String sortOrder) {
-		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
 
-		switch (sUriMatcher.match(uri)) {
-		case HISTORY:
-			qb.setTables(HISTORY_TABLE_NAME);
-			qb.setProjectionMap(sHistoryProjectionMap);
-			break;
+        switch (sUriMatcher.match(uri)) {
+            case HISTORY:
+                qb.setTables(HISTORY_TABLE_NAME);
+                qb.setProjectionMap(sHistoryProjectionMap);
+                break;
 
-		case HISTORY_ID:
-			qb.setTables(HISTORY_TABLE_NAME);
-			qb.setProjectionMap(sHistoryProjectionMap);
-			qb.appendWhere(History._ID + "=" + uri.getPathSegments().get(1));
-			break;
+            case HISTORY_ID:
+                qb.setTables(HISTORY_TABLE_NAME);
+                qb.setProjectionMap(sHistoryProjectionMap);
+                List path = uri.getPathSegments();
+                if (path != null) {
+                    String id = (String) path.get(1);
+                    if (id != null) {
+                        qb.appendWhere(History._ID + "=" + id);
+                    }
+                }
+                break;
 
-		default:
-			throw new IllegalArgumentException("Unknown URI " + uri);
-		}
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
 
-		// If no sort order is specified use the default
-		String orderBy;
-		if (TextUtils.isEmpty(sortOrder)) {
-			orderBy = CIDRHistory.History.DEFAULT_SORT_ORDER;
-		} else {
-			orderBy = sortOrder;
-		}
+        // If no sort order is specified use the default
+        String orderBy;
+        if (TextUtils.isEmpty(sortOrder)) {
+            orderBy = CIDRHistory.History.DEFAULT_SORT_ORDER;
+        } else {
+            orderBy = sortOrder;
+        }
 
-		// Get the database and run the query
-		Cursor c;
-		try {
-			SQLiteDatabase db = mOpenHelper.getReadableDatabase();
-			c = qb.query(db, projection, selection, selectionArgs, null, null, orderBy);
-		} catch (SQLiteException e) {
-			e.printStackTrace();
-			return null;
-		}
+        // Get the database and run the query
+        Cursor c;
+        try {
+            SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+            c = qb.query(db, projection, selection, selectionArgs, null, null, orderBy);
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+            return null;
+        }
 
-		// Tell the cursor what uri to watch, so it knows when its source data changes
-		c.setNotificationUri(getContext().getContentResolver(), uri);
-		return c;
-	}
+        // Tell the cursor what uri to watch, so it knows when its source data changes
+        Context context = getContext();
+        if (context != null) {
+            ContentResolver resolver = context.getContentResolver();
+            if (resolver != null) {
+                c.setNotificationUri(resolver, uri);
+            }
+        }
+        return c;
+    }
 
-	@Override
-	public String getType(Uri uri) {
-		switch (sUriMatcher.match(uri)) {
-		case HISTORY:
-			return History.CONTENT_TYPE;
+    @Override
+    public String getType(Uri uri) {
+        switch (sUriMatcher.match(uri)) {
+            case HISTORY:
+                return History.CONTENT_TYPE;
 
-		case HISTORY_ID:
-			return History.CONTENT_ITEM_TYPE;
+            case HISTORY_ID:
+                return History.CONTENT_ITEM_TYPE;
 
-		default:
-			throw new IllegalArgumentException("Unknown URI " + uri);
-		}
-	}
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+    }
 
-	public Uri insert(Uri uri, ContentValues initialValues) {
-		// Validate the requested uri
-		if (sUriMatcher.match(uri) != HISTORY) {
-			throw new IllegalArgumentException("Unknown URI " + uri);
-		}
+    public Uri insert(Uri uri, ContentValues initialValues) {
+        // Validate the requested uri
+        if (sUriMatcher.match(uri) != HISTORY) {
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
 
-		ContentValues values;
-		if (initialValues != null) {
-			values = new ContentValues(initialValues);
-		} else {
-			values = new ContentValues();
-		}
+        ContentValues values;
+        if (initialValues != null) {
+            values = new ContentValues(initialValues);
+        } else {
+            values = new ContentValues();
+        }
 
-		Long now = Long.valueOf(System.currentTimeMillis());
+        Long now = System.currentTimeMillis();
 
-		if (values.containsKey(History.CREATED_DATE) == false) {
-			values.put(History.CREATED_DATE, now);
-		}
+        if (!values.containsKey(History.CREATED_DATE)) {
+            values.put(History.CREATED_DATE, now);
+        }
 
-		if (values.containsKey(History.MODIFIED_DATE) == false) {
-			values.put(History.MODIFIED_DATE, now);
-		}
+        if (!values.containsKey(History.MODIFIED_DATE)) {
+            values.put(History.MODIFIED_DATE, now);
+        }
 
-		if (values.containsKey(History.IP) == false) {
-			values.put(History.IP, "");
-		}
+        if (!values.containsKey(History.IP)) {
+            values.put(History.IP, "");
+        }
 
-		if (values.containsKey(History.BITS) == false) {
-			values.put(History.BITS, 0);
-		}
+        if (!values.containsKey(History.BITS)) {
+            values.put(History.BITS, 0);
+        }
 
-		try {
-			SQLiteDatabase db = mOpenHelper.getWritableDatabase();
-			long rowId = db.insert(HISTORY_TABLE_NAME, null, values);
-			if (rowId > 0) {
-				Uri noteUri = ContentUris.withAppendedId(History.CONTENT_URI, rowId);
-				getContext().getContentResolver().notifyChange(noteUri, null);
-				keepCheck(db);
-				return noteUri;
-			}
-		} catch (SQLiteException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+        Uri noteUri = null;
+        try {
+            SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+            long rowId = db.insert(HISTORY_TABLE_NAME, null, values);
+            if (rowId > 0) {
+                noteUri = ContentUris.withAppendedId(History.CONTENT_URI, rowId);
+                notifyChange(uri);
+                keepCheck(db);
+            }
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+        }
+        return noteUri;
+    }
 
-	/**
-	 * Ensure that the database only keeps maxRows worth of records.
-	 * This keep the history from growing forever in size.
-	 * 
-	 * @param db a writable SQLiteDatabase
-	 */
-	private void keepCheck(SQLiteDatabase db) {
-		if (debug) Log.d(TAG,"keepCheck()");
-		long count;
-		try {
-			SQLiteStatement r = db.compileStatement("SELECT count(*) FROM " +
-					HISTORY_TABLE_NAME);
-			count = r.simpleQueryForLong(); 
-		} catch (SQLiteException e) {
-			e.printStackTrace();
-			return;
-		}
+    /**
+     * Ensure that the database only keeps maxRows worth of records.
+     * This keep the history from growing forever in size.
+     *
+     * @param db a writable SQLiteDatabase
+     */
+    private void keepCheck(SQLiteDatabase db) {
+        if (debug) Log.d(TAG, "keepCheck()");
+        long count;
+        try {
+            SQLiteStatement r = db.compileStatement("SELECT count(*) FROM " +
+                    HISTORY_TABLE_NAME);
+            count = r.simpleQueryForLong();
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+            return;
+        }
 
-		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
+        Context context = getContext();
+        if (context == null) {
+            return;
+        }
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
 
-		String historyEntries=sp.getString(Preferences.PREFERENCE_HISTORY_ENTRIES, "100");
-		int maxRows=Integer.parseInt(historyEntries);
-		if (count>maxRows) {
-			if (debug) Log.d(TAG,"keepCheck: greater than "+maxRows);
-			Cursor c;
-			try {
-				String[] projection={ History._ID };
-				c = db.query(HISTORY_TABLE_NAME, projection, null, null, null, null, History.DEFAULT_SORT_ORDER);
-				c.moveToPosition(maxRows);
-				while (!c.isAfterLast()) {
-					int rowId=c.getInt(0);
-					if (debug) Log.d(TAG,"keepCheck: need to delete "+rowId);
-					db.delete(HISTORY_TABLE_NAME, History._ID+"="+rowId,null);
-					c.moveToNext();
-				}
-				c.close();
-			} catch (SQLiteException e) {
-				e.printStackTrace();
-				return;
-			}
-		}
-	}
-	
-	@Override
-	public int delete(Uri uri, String where, String[] whereArgs) {
-		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
-		int count;
-		switch (sUriMatcher.match(uri)) {
-		case HISTORY:
-			count = db.delete(HISTORY_TABLE_NAME, where, whereArgs);
-			break;
+        String historyEntries = sp.getString(Preferences.PREFERENCE_HISTORY_ENTRIES, "100");
+        int maxRows = Integer.parseInt(historyEntries);
+        if (count > maxRows) {
+            if (debug) Log.d(TAG, "keepCheck: greater than " + maxRows);
+            Cursor c;
+            try {
+                String[] projection = {History._ID};
+                c = db.query(HISTORY_TABLE_NAME, projection, null, null, null, null, History.DEFAULT_SORT_ORDER);
+                c.moveToPosition(maxRows);
+                while (!c.isAfterLast()) {
+                    int rowId = c.getInt(0);
+                    if (debug) Log.d(TAG, "keepCheck: need to delete " + rowId);
+                    db.delete(HISTORY_TABLE_NAME, History._ID + "=" + rowId, null);
+                    c.moveToNext();
+                }
+                c.close();
+            } catch (SQLiteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-		case HISTORY_ID:
-			String noteId = uri.getPathSegments().get(1);
-			count = db.delete(HISTORY_TABLE_NAME, History._ID + "=" + noteId
-					+ (!TextUtils.isEmpty(where) ? " AND (" + where + ')' : ""), whereArgs);
-			break;
+    @Override
+    public int delete(Uri uri, String where, String[] whereArgs) {
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        int count = 0;
+        switch (sUriMatcher.match(uri)) {
+            case HISTORY:
+                count = db.delete(HISTORY_TABLE_NAME, where, whereArgs);
+                break;
 
-		default:
-			throw new IllegalArgumentException("Unknown URI " + uri);
-		}
+            case HISTORY_ID:
+                List path = uri.getPathSegments();
+                if (path != null) {
+                    String noteId = (String) path.get(1);
+                    count = db.delete(HISTORY_TABLE_NAME, History._ID + "=" + noteId
+                            + (!TextUtils.isEmpty(where) ? " AND (" + where + ')' : ""), whereArgs);
+                }
+                break;
 
-		getContext().getContentResolver().notifyChange(uri, null);
-		return count;
-	}
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
 
-	@Override
-	public int update(Uri uri, ContentValues values, String where, String[] whereArgs) {
-		if (debug) Log.d(TAG,"update("+uri+","+values+",,");
-		int count=0;
-		try {
-			SQLiteDatabase db = mOpenHelper.getWritableDatabase();
-			switch (sUriMatcher.match(uri)) {
-			case HISTORY:
-				count = db.update(HISTORY_TABLE_NAME, values, where, whereArgs);
-				break;
+        notifyChange(uri);
+        return count;
+    }
 
-			case HISTORY_ID:
-				String noteId = uri.getPathSegments().get(1);
-				count = db.update(HISTORY_TABLE_NAME, values, History._ID + "=" + noteId
-						+ (!TextUtils.isEmpty(where) ? " AND (" + where + ')' : ""), whereArgs);
-				break;
+    private void notifyChange(Uri uri) {
+        Context context = getContext();
+        if (context != null) {
+            context.getContentResolver().notifyChange(uri, null);
+        }
+    }
 
-			default:
-				throw new IllegalArgumentException("Unknown URI " + uri);
-			}
+    @Override
+    public int update(Uri uri, ContentValues values, String where, String[] whereArgs) {
+        if (debug) Log.d(TAG, "update(" + uri + "," + values + ",,");
+        int count = 0;
+        try {
+            SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+            switch (sUriMatcher.match(uri)) {
+                case HISTORY:
+                    count = db.update(HISTORY_TABLE_NAME, values, where, whereArgs);
+                    break;
 
-			getContext().getContentResolver().notifyChange(uri, null);
-		} catch (SQLException e) {
-			if (debug) Log.d(TAG,"SQL error: "+e.getLocalizedMessage());
-		}
-		return count;
-	}
+                case HISTORY_ID:
+                    List path = uri.getPathSegments();
+                    if (path != null) {
+                        String noteId = (String) path.get(1);
+                        count = db.update(HISTORY_TABLE_NAME, values, History._ID + "=" + noteId
+                                + (!TextUtils.isEmpty(where) ? " AND (" + where + ')' : ""), whereArgs);
+                    }
+                    break;
 
-	static {
-		sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-		sUriMatcher.addURI(CIDRHistory.AUTHORITY, "history", HISTORY);
-		sUriMatcher.addURI(CIDRHistory.AUTHORITY, "history/#", HISTORY_ID);
+                default:
+                    throw new IllegalArgumentException("Unknown URI " + uri);
+            }
 
-		sHistoryProjectionMap = new HashMap<String, String>();
-		sHistoryProjectionMap.put(History._ID, History._ID);
-		sHistoryProjectionMap.put(History.IP, History.IP);
-		sHistoryProjectionMap.put(History.BITS, History.BITS);
-		sHistoryProjectionMap.put(History.CREATED_DATE, History.CREATED_DATE);
-		sHistoryProjectionMap.put(History.MODIFIED_DATE, History.MODIFIED_DATE);
-	}
+            notifyChange(uri);
+        } catch (SQLException e) {
+            if (debug) Log.d(TAG, "SQL error: " + e.getLocalizedMessage());
+        }
+        return count;
+    }
+
+    static {
+        sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        sUriMatcher.addURI(CIDRHistory.AUTHORITY, "history", HISTORY);
+        sUriMatcher.addURI(CIDRHistory.AUTHORITY, "history/#", HISTORY_ID);
+
+        sHistoryProjectionMap = new HashMap<String, String>();
+        sHistoryProjectionMap.put(History._ID, History._ID);
+        sHistoryProjectionMap.put(History.IP, History.IP);
+        sHistoryProjectionMap.put(History.BITS, History.BITS);
+        sHistoryProjectionMap.put(History.CREATED_DATE, History.CREATED_DATE);
+        sHistoryProjectionMap.put(History.MODIFIED_DATE, History.MODIFIED_DATE);
+    }
 }
